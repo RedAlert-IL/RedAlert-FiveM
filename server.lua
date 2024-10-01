ff6ddc2295e9ac9e54e986a26d5deec3d7c80721
Danielb <103242366+Danielb23451@users.noreply.github.com>
2024-10-01 11:46:05 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,3 +1,5 @@
+local sentAlerts = {}
+
 CreateThread(function()
     while true do
         PerformHttpRequest("https://redalert.me/alerts", function(err, text, headers) 
@@ -5,16 +7,31 @@ CreateThread(function()
                 newJson = json.decode(text)
                 local object = {}
                 for k, v in pairs(newJson) do
-                    year,month,day,hour,min,sec = os.date('%Y-%m-%d %H:%M:%S', v['date']):match("(%d+)-(%d+)-(%d+) (%d+):(%d+):(%d+)")
-                    if os.time() - os.time({day=day,month=month,year=year,hour=hour,min=min,sec=sec}) <= 60 * 2.5 then
-                        object[k] = v
+                    year, month, day, hour, min, sec = os.date('%Y-%m-%d %H:%M:%S', v['date']):match("(%d+)-(%d+)-(%d+) (%d+):(%d+):(%d+)")
+                    if os.time() - os.time({day=day, month=month, year=year, hour=hour, min=min, sec=sec}) <= 60 * 2.5 then
+                        if not sentAlerts[v['id']] then 
+                            object[k] = v
+                            sentAlerts[v['id']] = true 
+                        end
                     end
                 end
 
-                TriggerClientEvent('newAlert', -1, object)
+                if next(object) ~= nil then 
+                    TriggerClientEvent('newAlert', -1, object)
+                end
             end
         end)
 
         Wait(3000)
     end
 end)
+
+RegisterCommand('testalert', function(source, args, rawCommand)
+    local alertObjects = {
+        { id = "alert", area = "אזור תעשייה רמת הגולן " },
+        { id = "alert", area = "בדיקה" },
+
+    }
+
+    TriggerClientEvent('newAlert', -1, alertObjects)
+end, false)
